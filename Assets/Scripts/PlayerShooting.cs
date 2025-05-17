6f701b80c19a04bcac14b5e6121c2086dd8ad51c
Yompol-Lab <juanpabloabraham23gmail.com>
2025-05-17 19:19:52 -0300

    intento disparo

---
+++ Assets/Scripts/PlayerShooting.cs
@@ -0,0 +1,52 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerShooting : MonoBehaviour
+{
+    public GameObject bulletPrefab;
+    public Transform gunLeft;
+    public Transform gunRight;
+    public float fireRate = 0.1f; 
+    public float bulletSpeed = 50f;
+
+    private bool isShooting = false;
+    private float shootTimer;
+
+    void Update()
+    {
+        if (isShooting)
+        {
+            shootTimer -= Time.deltaTime;
+            if (shootTimer <= 0f)
+            {
+                Shoot();
+                shootTimer = fireRate;
+            }
+        }
+    }
+
+    public void OnFire(InputAction.CallbackContext context)
+    {
+        if (context.started)
+            isShooting = true;
+        else if (context.canceled)
+            isShooting = false;
+    }
+
+    void Shoot()
+    {
+       
+        FireBulletFrom(gunLeft);
+        FireBulletFrom(gunRight);
+    }
+
+    void FireBulletFrom(Transform gun)
+    {
+        GameObject bullet = Instantiate(bulletPrefab, gun.position, gun.rotation);
+        Rigidbody rb = bullet.GetComponent<Rigidbody>();
+        if (rb != null)
+        {
+            rb.linearVelocity = gun.forward * bulletSpeed;
+        }
+    }
+}
